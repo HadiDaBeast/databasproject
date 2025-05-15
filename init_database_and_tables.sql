f7d5913c0f46e88d908408f27e3b2a876403ae0d
Jolliy <jojw21@student.bth.se>
2025-05-15 12:25:39 +0200

    init database and tables

---
+++ init_database_and_tables.sql
@@ -0,0 +1,32 @@
+
+-- initiate database and tables
+
+drop database if exists course_overview;
+Create database course_overview;
+
+use course_overview;
+
+drop table if exists students;
+drop table if exists courses;
+drop table if exists registrations;
+
+create table students (
+	person_nr varchar(12) primary key,
+    student_name varchar(50),
+    program varchar(20),
+    completed_hp int
+    );
+create table courses (
+	course_code varchar(20) primary key, 
+    course_hp varchar(20),
+    requirements varchar(20)
+    );
+create table registrations (
+	person_nr varchar(12), 
+	course_code varchar(50), 
+	completed_hp_course int, 
+	course_type varchar(20),
+	foreign key (person_nr) references students(person_nr), 
+	foreign key (course_code) references courses(course_code)
+    );
+
