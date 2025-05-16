e92721882454cda8d009fd5f44e0a5d29cb4dcd9
Hadi Abou Zahra <guntrix3rhd@gmail.com>
2025-05-16 15:05:51 +0200

    Added procedure for courses

---
+++ Course Procedures.sql
@@ -0,0 +1,30 @@
+use course_overview;
+drop procedure if exists add_course;
+drop procedure if exists remove_course;
+
+DELIMITER //
+CREATE procedure add_course(in code varchar(12), in hp varchar(50), in type varchar(20), in requirements varchar(20))
+begin 
+	declare present int;
+	select count(course_code) into present from courses where courses.course_code = code;
+    if present = 0 then
+		insert into courses (course_code, course_hp, course_type, requirements) values
+        (code, hp, type, requirements);
+    end if;
+    
+end;
+// DELIMITER ;
+
+DELIMITER //
+CREATE procedure remove_course(in code varchar(12))
+begin 
+	delete from courses where courses.course_code = code;
+end;
+// DELIMITER ;
+
+call add_course('DV1574', '6', '100000000', '000000000000000000000000');
+call add_course('MA1486', '4', '0000000010000', '000000000000000000000000');
+call add_course('PA1461', '8', '000000000000000000000000');
+
+
+SELECT * from courses;
--- init_database_and_tables.sql
+++ init_database_and_tables.sql
@@ -19,14 +19,13 @@ create table students (
 create table courses (
 	course_code varchar(20) primary key, 
     course_hp varchar(20),
+    course_type varchar(20),
     requirements varchar(20)
     );
 create table registrations (
 	person_nr varchar(12), 
 	course_code varchar(50), 
 	completed_hp_course int, 
-	course_type varchar(20),
 	foreign key (person_nr) references students(person_nr), 
 	foreign key (course_code) references courses(course_code)
     );
-
