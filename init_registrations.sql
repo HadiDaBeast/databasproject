d9551014a98e4979bfefbe08a5d4dd13027d2806
Hadi Abou Zahra <guntrix3rhd@gmail.com>
2025-05-22 11:47:54 +0200

    Now you can see how many HP a course has

--- init_registrations.sql
+++ init_registrations.sql
@@ -1,1 +1,3 @@
 use course_overview;
+
+call add_course("TestKurs", 69, "LOL");
--- ui.py
+++ ui.py
@@ -32,7 +32,11 @@ def get_registered_courses():
     if personnumber:
         student_name = personnumber
         cur.execute("SELECT * FROM registrations WHERE person_nr = %s", (personnumber,))
-        result = [(x[1], x[2]) for x in cur.fetchall()]
+        result = [[x[1], x[2], 0] for x in cur.fetchall()]
+        for i in result:
+            cur.execute("SELECT course_hp FROM courses WHERE course_code = %s", (i[0],))
+            course = cur.fetchall()
+            i[2] = course[0][0]
     return render_template('index.html', current_name = student_name, Registered=result)
         
 @app.route('/add_student', methods=['post'])
@@ -104,7 +108,11 @@ def add_registrations():
     cur.callproc("add_registrations", (personnumber, course))
     cnx.commit()
     cur.execute("SELECT * FROM registrations WHERE person_nr = %s", (student_name,))
-    result = [(x[1], x[2]) for x in cur.fetchall()]
+    result = [[x[1], x[2], 0] for x in cur.fetchall()]
+    for i in result:
+        cur.execute("SELECT course_hp FROM courses WHERE course_code = %s", (i[0],))
+        course = cur.fetchall()
+        i[2] = course[0][0]
     return render_template('index.html', current_name = student_name, Registered=result)
 
 @app.route('/update_registrations_hp', methods=['POST'])
@@ -117,7 +125,11 @@ def update_registrations_hp():
     cur.callproc("update_registrations_hp", (personnumber, course, hp))
     cnx.commit()
     cur.execute("SELECT * FROM registrations WHERE person_nr = %s", (student_name,))
-    result = [(x[1], x[2]) for x in cur.fetchall()]
+    result = [[x[1], x[2], 0] for x in cur.fetchall()]
+    for i in result:
+        cur.execute("SELECT course_hp FROM courses WHERE course_code = %s", (i[0],))
+        course = cur.fetchall()
+        i[2] = course[0][0]
     return render_template('index.html', current_name = student_name, Registered=result)
 
 @app.route('/remove_registrations', methods=['POST'])
@@ -129,8 +141,66 @@ def remove_registrations():
     cur.callproc("remove_registrations", (personnumber, course))
     cnx.commit()
     cur.execute("SELECT * FROM registrations WHERE person_nr = %s", (student_name,))
-    result = [(x[1], x[2]) for x in cur.fetchall()]
+    result = [[x[1], x[2]] for x in cur.fetchall()]
+    
+    for i in result:
+        cur.execute("SELECT course_hp FROM courses WHERE course_code = %s", (i[0],))
+        course = cur.fetchall()
+        print(course)
+        i[2] = course[0]
+        
     return render_template('index.html', current_name = student_name, Registered=result)
+
+@app.route('/add_course', methods=['POST'])
+def add_course():
+    global student_name
+    global result
+    course = request.form.get("course")
+    hp = int(request.form.get("hp"))
+    type = request.form.get("type")
+    cur.callproc("add_course", (course, hp, type))
+    cnx.commit()
+    return render_template('index.html', current_name = student_name, Registered=result)
+
+@app.route('/remove_course', methods=['POST'])
+def remove_course():
+    global student_name
+    global result
+    course = request.form.get("course")
+    cur.callproc("remove_course", (course,))
+    cnx.commit()
+    return render_template('index.html', current_name = student_name, Registered=result)
+
+@app.route('/update_course_code', methods=['POST'])
+def update_course_code():
+    global student_name
+    global result
+    course = request.form.get("course")
+    new = request.form.get("new")
+    cur.callproc("update_course_code", (course, new))
+    cnx.commit()
+    return render_template('index.html', current_name = student_name, Registered=result)
+
+@app.route('/update_course_hp', methods=['POST'])
+def update_course_hp():
+    global student_name
+    global result
+    course = request.form.get("course")
+    hp = request.form.get("hp")
+    cur.callproc("update_course_hp", (course, hp))
+    cnx.commit()
+    return render_template('index.html', current_name = student_name, Registered=result)
+
+@app.route('/update_course_type', methods=['POST'])
+def update_course_type():
+    global student_name
+    global result
+    course = request.form.get("course")
+    type = request.form.get("type")
+    cur.callproc("update_course_type", (course, type))
+    cnx.commit()
+    return render_template('index.html', current_name = student_name, Registered=result)
+
 
 if __name__ == '__main__':
    app.run(debug=True) 
